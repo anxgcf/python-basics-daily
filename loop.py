c4ce048484d59557d6b8784c988240602883e9a8
anjana <anjanxa@gmail.com>
2025-11-23 18:15:09 +0530

    Add programs to print numbers using loops

--- loop.py
+++ loop.py
@@ -1,2 +1,25 @@
+#Prog 1: print 1 to 10 using for loop
 for i in range(1,11):
   print(i)
+#-------------------------------------
+
+#Prog 2: print 1 to 10 using while loop
+i=1
+while i<=10:
+  print(i)
+  i+=1
+#-------------------------------------
+
+#Prog 3: print even numbers using for loop
+for i in range(1,11):
+  if i%2==0:
+    print(i)
+#-------------------------------------
+
+#Prog 4: print even numbers using while loop
+i=1
+while i<=10:
+  if i%2==0:
+    print(i)
+  i+=1
+  
