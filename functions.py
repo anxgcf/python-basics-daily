e96705ca2d55cda5457bdf8d6a2843053bba6541
anjana <anjanxa@gmail.com>
2025-11-23 18:54:17 +0530

    Refactor functions for name printing and even checking

--- functions.py
+++ functions.py
@@ -1,20 +1,32 @@
-#Function 1
-def print_name(name):
-  for i in range(5):
-    print(name)
-name1=input("enter your name:")
-print_name(name1)
+# Function 1: Print your name 5 times
+def print_name():
+    for i in range(5):
+        print("Alice")
 
-#Function 2
-def check_even(n):
-  if n%2==0:
-    print(even)
-  else:
-    print(odd)
-number=int(input("Enter a number:"))
-check_even(number)
+# Function 2: Check if a number is even
+def is_even(n):
+    if n%2==0:
+        return True
+    return False
 
-#Function 3
+# Function 3: Add two numbers
 def add(a,b):
-  result(a+b)
-print(add(7,3))
+    return a+b
+
+# -------------------------
+# Function Calls (Testing)
+# -------------------------
+
+print("Printing name 5 times:")
+print_name()
+
+print("------------------------")
+
+print("Checking if 10 is even:")
+print(is_even(10))  
+
+print("------------------------")
+
+print("Adding 10 and 20:")
+print(add(10, 20))  
+
