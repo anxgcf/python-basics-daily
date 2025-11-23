9c57e81e34baed4a8b5fdb179835ef2811672f28
anjana <anjanxa@gmail.com>
2025-11-23 18:35:51 +0530

    Implement three utility functions in functions.py
    Added three functions: print_name to print a name multiple times, check_even to determine if a number is even or odd, and add to sum two numbers.

---
+++ functions.py
@@ -0,0 +1,20 @@
+#Function 1
+def print_name(name):
+  for i in range(5):
+    print(name)
+name1=input("enter your name:")
+print_name(name1)
+
+#Function 2
+def check_even(n):
+  if n%2==0:
+    print(even)
+  else:
+    print(odd)
+number=int(input("Enter a number:"))
+check_even(number)
+
+#Function 3
+def add(a,b):
+  result(a+b)
+print(add(7,3))
